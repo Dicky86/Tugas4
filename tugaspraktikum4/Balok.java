5da812ca2325f8ee81e39a9f872181ab1c67bc55
Moh. Dicky Purnama <114645106+Dicky86@users.noreply.github.com>
2023-11-01 22:25:30 +0700

    Add files via upload

---
+++ tugaspraktikum4/Balok.java
@@ -0,0 +1,23 @@
+package tugaspraktikum4;
+
+public class Balok extends Perhitungan{
+    public double p, l, t;
+
+    public Balok(double p, double l, double t) {
+        this.p = p;
+        this.l = l;
+        this.t = t;
+    }
+
+    @Override
+    public double Volume() {
+        //rumus volume balok = p*l*t
+        return p * l * t;
+    }
+
+    @Override
+    public double LuasPermukaan() {
+        //rumus luas permukaan balok = 2*(p*l + p*t + l*t)
+        return 2 * (p * l + p * t + l * t);
+    }
+}
---
+++ tugaspraktikum4/Perhitungan.java
@@ -0,0 +1,11 @@
+package tugaspraktikum4;
+
+public class Perhitungan {
+    public double Volume() {
+        return 0;
+    }
+    public double LuasPermukaan() {
+        return 0;
+    }
+}
+
